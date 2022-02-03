401ae1216642a36d58f582f70b6e0a13d04c7779
erel42 <Erelafu@gmail.com>
2022-02-03 14:29:24 +0200

    small updates

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
+import os
 
 pygame.init()
 screen = pygame.display.set_mode([1500, 1000], flags=pygame.FULLSCREEN)
 exit_game = False
 tile_list = [[]]
 clock = pygame.time.Clock()
-assets_path = 'C:\Users\erela\OneDrive\Documents\GitHub\Final_Project\Assets'
-save_path = 'C:\Users\erela\OneDrive\Documents\GitHub\Final_Project\Saves'
+assets_path = 'Assets'
+save_path = 'Saves'
+cur_game_save_path = None
 
 
 class Tile:
@@ -27,11 +29,31 @@ class RoadTile(Tile):
         self.set_texture(orientation)  # Can be cross, vertical, horizontal, T_down, T_left, T_right and T_up
 
 
+# Loads a tile from file
+def load_tile(x: int, y: int):
+
+
+# Unloads and saves a tile
+def unload_tile(x: int, y: int):
+
+
+# Generates the starting map
+def generate_base_tiles():
+
+
+
 def create_new_save(name: str):
-    
+    global cur_game_save_path
+    # Creating a new directory
+    cur_game_save_path = save_path + '\\' + name
+    os.mkdir(cur_game_save_path)
+    generate_base_tiles()
 
 
-def load_save():
+
+def load_save(name: str):
+    global cur_game_save_path
+    cur_game_save_path = save_path + '\\' + name
 
 
 def event_handler():
@@ -49,7 +71,7 @@ def close_game():
     print('Exiting game without saving')
 
 
-def gameloop():
+def game_loop():
     while not exit_game:
         event_handler()
 
@@ -58,4 +80,4 @@ def gameloop():
 
 if __name__ == '__main__':
     main_menu()
-    gameloop()
+    game_loop()
